4a5b5909478ddbae2bda7af13ec339035efd59f9
Matheus Gimenez <126830969+Gimenezmt@users.noreply.github.com>
2024-04-16 10:44:05 -0300

    Create databaseQuiz.js

---
+++ databaseQuiz.js
@@ -0,0 +1,21 @@
+// databaseQuiz.js
+
+const databaseQuestions = [
+  {
+    question: "O que é SQL?",
+    options: ["Structured Query Language", "Simple Query Language", "Sequential Query Language"],
+    answer: "Structured Query Language"
+  },
+  {
+    question: "Qual é a função do comando SELECT?",
+    options: ["Inserir dados em uma tabela", "Atualizar dados em uma tabela", "Selecionar dados de uma tabela"],
+    answer: "Selecionar dados de uma tabela"
+  },
+  {
+    question: "O que é uma chave primária em um banco de dados relacional?",
+    options: ["Uma coluna que contém valores únicos para cada linha na tabela", "Uma coluna que permite valores nulos", "Uma coluna que armazena valores de texto"],
+    answer: "Uma coluna que contém valores únicos para cada linha na tabela"
+  }
+];
+
+export default databaseQuestions;
