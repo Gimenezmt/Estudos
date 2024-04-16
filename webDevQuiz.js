912fccb93d61d6d9b757aec033d377bfbcc68e80
Matheus Gimenez <126830969+Gimenezmt@users.noreply.github.com>
2024-04-16 10:50:04 -0300

    Create webDevQuiz.js

---
+++ webDevQuiz.js
@@ -0,0 +1,21 @@
+// webDevQuiz.js
+
+const webDevQuestions = [
+  {
+    question: "Qual é a linguagem de marcação utilizada para estruturar páginas web?",
+    options: ["HTML", "CSS", "JavaScript"],
+    answer: "HTML"
+  },
+  {
+    question: "Qual é a linguagem de estilização utilizada para definir a apresentação de páginas web?",
+    options: ["HTML", "CSS", "JavaScript"],
+    answer: "CSS"
+  },
+  {
+    question: "Qual é a linguagem de programação utilizada para adicionar interatividade em páginas web?",
+    options: ["HTML", "CSS", "JavaScript"],
+    answer: "JavaScript"
+  }
+];
+
+export default webDevQuestions;
